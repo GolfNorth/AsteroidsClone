c7e0362a88853765c3a2944a9eb949d032b7e089
GolfNorth <golf155@outlook.com>
2020-09-11 13:51:47 +0500

    Update BulletData.cs

--- Assets/Scripts/Data/BulletData.cs
+++ Assets/Scripts/Data/BulletData.cs
@@ -3,15 +3,12 @@
 namespace AsteroidsClone
 {
     [CreateAssetMenu(fileName = "BulletData")]
-    public sealed class BulletData : ScriptableObject
+    public sealed class BulletData : ViewData
     {
-        [SerializeField] private GameObject polygonalPrefab;
-        [SerializeField] private GameObject spritePrefab;
+        [Header("Settings")]
         [SerializeField] private float range;
         [SerializeField] private float speed;
 
-        public GameObject PolygonalPrefab => polygonalPrefab;
-        public GameObject SpritePrefab => spritePrefab;
         public float Range => range;
         public float Speed => speed;
     }
