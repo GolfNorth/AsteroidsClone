e140c50194164ccce741035dc79b4f56c86f5976
GolfNorth <golf155@outlook.com>
2020-09-11 15:21:48 +0500

    Replaced fields by autoproperty

--- Assets/Scripts/Models/Model.cs
+++ Assets/Scripts/Models/Model.cs
@@ -4,14 +4,9 @@ namespace AsteroidsClone
 {
     public abstract class Model
     {
-        protected ViewMode viewMode;
-        protected Vector2 position;
-        protected Vector2 velocity;
-        protected float angle;
-
-        public ViewMode ViewMode { get => viewMode; set => viewMode = value; }
-        public Vector2 Position { get => position; set => position = value; }
-        public Vector2 Velocity { get => velocity; set => velocity = value; }
-        public float Angle { get => angle; set => angle = value; }
+        public ViewMode ViewMode { get; set; }
+        public Vector2 Position { get; set; }
+        public Vector2 Velocity { get; set; }
+        public float Angle { get; set; }
     }
 }
